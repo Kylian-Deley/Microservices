f9c835fcac5a982eef56fd9717761c99af57a797
Kylian <kylian.deley@gmail.com>
2024-09-09 17:36:40 +0200

    test running services

--- back/services/client-service/app.js
+++ back/services/client-service/app.js
@@ -9,6 +9,15 @@ app.get('/clients', (req, res) => {
     res.send([{ id: 1, name: "Client A" }, { id: 2, name: "Client B" }]);
 });
 
+app.get('/test-mongo', async (req, res) => {
+  try {
+      const result = await mongoose.connection.db.admin().ping();
+      res.json({ message: "MongoDB connection is successful", result });
+  } catch (error) {
+      res.status(500).json({ message: "MongoDB connection failed", error });
+  }
+});
+
 app.listen(PORT, () => {
     console.log(`Client Service is running on port ${PORT}`);
 });
