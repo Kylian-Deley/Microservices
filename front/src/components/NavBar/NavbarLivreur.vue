aaab288bf39780ea76ffb014da67ac86de88c6f1
Jeremy <jeremy@sensei-france.fr>
2024-11-07 14:21:35 +0100

    fix: restaurant name

--- front/src/components/NavBar/NavbarLivreur.vue
+++ front/src/components/NavBar/NavbarLivreur.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="bg-gradient-to-r from-indigo-700 to-purple-600 bg-opacity-90 backdrop-blur-md fixed w-full p-5 text-white shadow-lg z-50">
         <div class="max-w-6xl mx-auto flex justify-between items-center">
-            <a href="/livraison" class="text-3xl font-bold tracking-wide text-gray-100 hover:text-yellow-400 transition">🚚 Livraison</a>
+            <a href="/livraison" class="text-3xl font-bold tracking-wide text-gray-100 hover:text-yellow-400 transition">🚚 Salt Bae</a>
             <div class="space-x-6 flex items-center">
                 <router-link to="/livrees" class="hover:text-yellow-400 text-lg font-medium transition">Commandes Livrées</router-link>
             </div>
